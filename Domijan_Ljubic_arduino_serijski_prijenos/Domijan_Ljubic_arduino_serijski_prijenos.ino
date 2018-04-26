f04a13603809fda06290661b56ebfc737641f600
DavidDomijan <36442082+DavidDomijan@users.noreply.github.com>
2018-04-26 12:53:07 +0200

    Add files via upload

---
+++ Domijan_Ljubic_arduino_serijski_prijenos/Domijan_Ljubic_arduino_serijski_prijenos.ino
@@ -0,0 +1,37 @@
+
+int inPin = 3;
+int x[7];
+void setup() {
+  // put your setup code here, to run once:
+pinMode(inPin, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+val = digitalRead(inPin);
+if(digitalRead)== high
+  {
+    if(digitalRead(inPin)== low )
+      {
+        delay(500);
+        //start
+        if(digitalRead(inPin) == low)
+          {
+            delay(1000);
+            // upisivanje bitova
+            for(int i=7;i>=0;i--)
+            {
+              x[i]== digitalRead(inPin);
+              delay(1000);
+            }
+           
+          }
+      }
+  }
+
+  // prebacivanje 8 BITNOG zapisa u ASCII
+  
+
+  
+}
---
+++ Domijan_Ljubic_kulusicSerijska/Domijan_Ljubic_kulusicSerijska.ino
@@ -0,0 +1,41 @@
+int led=10;
+
+void setup() {
+   pinMode(10,OUTPUT);
+digitalWrite(led,HIGH);
+
+}
+
+void loop() {
+//cekanje
+digitalWrite(led,HIGH);
+
+//start bit
+digitalWrite(led,LOW);
+delay(1000);
+
+//slovo a
+digitalWrite(led,LOW);
+delay(1000);
+digitalWrite(led,HIGH);
+delay(1000);
+digitalWrite(led,HIGH);
+delay(1000);
+digitalWrite(led,LOW);
+delay(1000);
+digitalWrite(led,LOW);
+delay(1000);
+digitalWrite(led,LOW);
+delay(1000);
+digitalWrite(led,LOW);
+delay(1000);
+digitalWrite(led,HIGH);
+delay(1000);
+
+//stop bit
+digitalWrite(led,HIGH);
+delay(10000);
+
+
+}
+
